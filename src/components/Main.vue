badbc7ea7fd3f47368e95f06a361314e1fb67c16
xinnix <xinnix2000@gmail.com>
2017-05-28 22:10:33 +0800

    init

---
+++ src/components/Main.vue
@@ -0,0 +1,71 @@
+<template>
+<div class="container">
+
+  <div class="hello row">
+    <div class="logo">
+      <img src="../assets/timg.jpeg" alt="" />
+
+    </div>
+    <div class="title">
+      <h1>食色代码</h1>
+      <h3><small>Beautiful Coding</small></h3>
+    </div>
+
+  </div>
+  <div class="content row">
+    <video-item class="col-md-4 item"></video-item>
+    <video-item class="col-md-4 item"></video-item>
+    <video-item class="col-md-4 item"></video-item>
+    <video-item class="col-md-4 item"></video-item>
+
+  </div>
+</div>
+
+</template>
+
+<script>
+import VideoItem from './VideoItem';
+
+export default {
+  components: {
+    VideoItem,
+  },
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  display: flex;
+  display: -webkit-flex; /* Safari */
+  justify-content: flex-start;
+  align-items: baseline;
+  padding-left: 2rem;
+  /*border-bottom: 1px solid #ccc;*/
+}
+h1, h2 {
+  font-weight: normal;
+  margin-bottom: 0;
+
+}
+h3{
+  margin-top: 0;
+
+}
+.content{
+  text-align: left;
+  margin: 0 auto;
+  padding-top: 1rem;
+}
+.item{
+  margin-bottom: 1rem;
+}
+.logo img{
+  width:5rem;
+}
+</style>
---
+++ src/components/VideoItem.vue
@@ -0,0 +1,60 @@
+<template>
+
+  <div class="video-item">
+    <div class="video-title">
+      食色代码-找人
+    </div>
+    <div class="video-content">
+      <img src="../assets/1.jpeg" alt="" />
+    </div>
+    <div class="video-info">
+      这一期节目主要为了食色代码寻觅一个主持人，在师大、政法新校区开展
+    </div>
+    <!-- <div class="video-footer">
+      点赞
+    </div> -->
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      msg: 'Welcome to Your Vue.js App',
+    };
+  },
+};
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+.hello{
+  display: flex;
+  display: -webkit-flex; /* Safari */
+  justify-content: flex-start;
+  align-items: baseline;
+  /*padding-left: 2rem;*/
+}
+h1, h2 {
+  font-weight: normal;
+  margin-bottom: 0;
+
+}
+h3{
+  margin-top: 0;
+
+}
+.video-item{
+
+}
+.video-title{
+  font-size: 2rem;
+  color: #777;
+}
+.video-content img{
+  width: 100%;
+}
+.logo img{
+  width:5rem;
+}
+</style>
